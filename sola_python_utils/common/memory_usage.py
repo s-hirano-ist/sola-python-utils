0238a47b976f4dfbc727ca34c34456d174cfa56f
s-hirano-ist <solucky0725@icloud.com>
2023-11-18 11:36:31 +0900

    fix: import relative error

--- sola_python_utils/common/memory_usage.py
+++ sola_python_utils/common/memory_usage.py
@@ -1,14 +1,17 @@
 import subprocess
 import time
 
-from line_notify import notify
+from .line_notify import notify
 
-if __name__ == "__main__":
+
+def memory_usage():
     while True:
         args = ["free", "-g"]
         res = subprocess.check_output(args)
         conv = res.decode("utf-8").split()
-        string = f"{conv[6]} {conv[8]}/{conv[7]} {int(int(conv[8]) / int(conv[7]) * 100)}%"
+        string = (
+            f"{conv[6]} {conv[8]}/{conv[7]} {int(int(conv[8]) / int(conv[7]) * 100)}%"
+        )
         print(string)
         notify(string)
         time.sleep(3)
